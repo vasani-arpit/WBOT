860e793e482bbce9c5e68eb8a616035e3dd5e12a
Arpit Vasani <vasani.arpit@gmail.com>
2019-05-14 16:18:29 +0530

    Updating puppeteer to puppeteer-core to reduce download.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const _cliProgress = require('cli-progress');
 require("./welcome");
 var spinner = require("./step");
