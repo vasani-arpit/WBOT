b770fc2f4df8e5e9887f9650893af67dd898531f
Adam Burns <burns@mict-international.org>
2020-07-22 10:45:46 +0200

    adds functions to read in json in chat filestore and write updates

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 var path = require("path");
 const mime = require('mime');
 var fs = require("fs");
+var constants = require("./constants");
+
 this.injection = function (filename) {
     return new Promise((resolve, reject) => {
         var filepath = path.join(__dirname, filename);
@@ -23,6 +25,58 @@ this.externalInjection = function (filename) {
         });
     });
 }
+
+this.getDirStore = function (dir) {
+    return new Promise((resolve, reject) => {
+        //console.log("reading file from " + dir);
+
+        var store = {};
+        store.chat = [];
+
+	fs.readdir(dir, (err, files) => {
+            if (err) {
+		console.log('found err' + err);
+                return reject(err);
+            } else {
+                // files object contains all file names in store dir
+                //
+		//console.log('Files:' + files);
+                files.forEach(file => {
+                    try {
+			const data = fs.readFileSync(path.join(dir, file), 'utf8');
+                        //console.log(data);
+                        store.chat.push(JSON.parse(data));
+			//console.log(store);
+                    } catch (err) {
+			return reject(err);
+                    }
+                });
+		//console.log('Final Store:\n' + JSON.stringify(store,null,2));
+		const resolution = Buffer.from(JSON.stringify(store));
+		//console.log(resolution);
+		resolve(resolution);
+	    }
+        });
+    });
+}
+
+this.putDirStoreEntry = function (file, json) {
+    return new Promise((resolve, reject) => {
+
+        //console.log('About to open writeStream: ' + path.join(constants.DEFAULT_CHATID_STORE_DIR, file + '.json'));
+
+        let writer = fs.createWriteStream(path.join(constants.DEFAULT_CHATID_STORE_DIR, file + '.json'))
+        .on('error', function (err) {
+            console.log('A write stream error' + err);
+            reject(err);
+        });
+        writer.write(json);
+        resolve(path.join(dir, file));
+    });
+}
+
+
+
 
 this.getFileInBase64 = function (filename) {
     return new Promise((resolve, reject) => {
