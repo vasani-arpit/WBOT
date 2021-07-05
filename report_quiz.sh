4d786fe1eac0e7976f97831b1ccc766c8c93632a
Adam Burns <burns@mict-international.org>
2021-07-05 11:14:43 +0200

    adds report_quiz script

--- report_quiz.sh
+++ report_quiz.sh
@@ -2,8 +2,10 @@
 
 CHATID_FILE=${CHATID_FILE:-$1}
 CHATID_FILE=${CHATID_FILE:-"chatIdStore/491704552266@c.us.json"}
-PUSHNAME=$(jq '.contact.pushname' ${CHATID_FILE})
 NAME=$(jq '.contact.name' ${CHATID_FILE})
+SHORTNAME=$(jq '.contact.shortname' ${CHATID_FILE})
+PUSHNAME=$(jq '.contact.pushname' ${CHATID_FILE})
+FORMATTEDNAME=$(jq '.contact.formattedName' ${CHATID_FILE})
 NUMBER=$(jq '.contact.id' ${CHATID_FILE})
 CORRECT_ANSWERS=$(jq '.quiz[] | select(.id|(test("Pearl03[1-4]"))) | select(.isCompleted="true")' ${CHATID_FILE} | jq '.question[].reply.isCorrect' | grep true | wc -l)
-echo "${CORRECT_ANSWERS} ${PUSHNAME} ${NAME} ${NUMBER}"
+echo "${CORRECT_ANSWERS} name: ${NAME} shortname: ${SHORTNAME} pushname: ${PUSHNAME} formattedName: ${FORMATTEDNAME} id: ${NUMBER}"
