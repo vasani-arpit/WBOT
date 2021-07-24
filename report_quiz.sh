da50f456966b645e74115e81945b60e1c93b4d88
Adam Burns <burns@mict-international.org>
2021-07-24 17:46:34 +0200

    for second results

--- report_quiz.sh
+++ report_quiz.sh
@@ -7,5 +7,7 @@ SHORTNAME=$(jq '.contact.shortname' ${CHATID_FILE})
 PUSHNAME=$(jq '.contact.pushname' ${CHATID_FILE})
 FORMATTEDNAME=$(jq '.contact.formattedName' ${CHATID_FILE})
 NUMBER=$(jq '.contact.id' ${CHATID_FILE})
-CORRECT_ANSWERS=$(jq '.quiz[] | select(.id|(test("Pearl03[1-4]"))) | select(.isCompleted="true")' ${CHATID_FILE} | jq '.question[].reply.isCorrect' | grep true | wc -l)
+#CORRECT_ANSWERS=$(jq '.quiz[] | select(.id|(test("Pearl03[1-4]"))) | select(.isCompleted="true")' ${CHATID_FILE} | jq '.question[].reply.isCorrect' | grep true | wc -l)
+#CORRECT_ANSWERS=$(jq '.quiz[] | select(.id|(test("Pearl03[5-8]"))) | select(.isCompleted="true")' ${CHATID_FILE} | jq '.question[].reply.isCorrect' | grep true | wc -l)
+CORRECT_ANSWERS=$(jq '.quiz[] | select(.id|(test("Pearl03[5-9]"))) | select(.isCompleted="true")' ${CHATID_FILE} | jq '.question[].reply.isCorrect' | grep true | wc -l)
 echo "${CORRECT_ANSWERS} name: ${NAME} shortname: ${SHORTNAME} pushname: ${PUSHNAME} formattedName: ${FORMATTEDNAME} id: ${NUMBER}"
