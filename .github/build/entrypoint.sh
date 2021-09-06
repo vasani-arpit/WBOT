0daef613ce7b709ad38728d0b568fa843163f40d
Arpit Vasani <vasani.arpit@gmail.com>
2021-09-06 17:53:45 +0530

    adding file in zip

--- .github/build/entrypoint.sh
+++ .github/build/entrypoint.sh
@@ -4,6 +4,6 @@ set -x
 npm install
 npm run pack
 
-zip "WBOT-linux.zip"  bot.json github.png wbot-linux
-zip "WBOT-mac.zip"  bot.json github.png wbot-macos
-zip "WBOT-win.zip"  bot.json github.png wbot-win.exe
+zip "WBOT-linux.zip"  bot.json github.png github-two.png wbot-linux
+zip "WBOT-mac.zip"  bot.json github.png github-two.png wbot-macos
+zip "WBOT-win.zip"  bot.json github.png github-two.png wbot-win.exe
