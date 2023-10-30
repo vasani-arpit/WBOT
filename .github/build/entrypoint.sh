aec85e6754409c790f130d5771fcc361c6a8ccb2
Arpit Vasani <vasani.arpit@gmail.com>
2023-10-30 11:35:40 +0530

    CI changes

--- .github/build/entrypoint.sh
+++ .github/build/entrypoint.sh
@@ -5,6 +5,6 @@ pwd && ls -la
 npm install
 npm run pack
 
-zip "WBOT-linux.zip"  bot.json github.png github-two.png wbot-linux
-zip "WBOT-mac.zip"  bot.json github.png github-two.png wbot-macos
-zip "WBOT-win.zip"  bot.json github.png github-two.png wbot-win.exe
+zip "WBOT-linux.zip"  bot.json github.png github-two.png wbot-linux file_example_MP3_700KB.mp3
+zip "WBOT-mac.zip"  bot.json github.png github-two.png wbot-macos file_example_MP3_700KB.mp3
+zip "WBOT-win.zip"  bot.json github.png github-two.png wbot-win.exe file_example_MP3_700KB.mp3
